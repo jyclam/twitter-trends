5b2a3e19af80e2d766bcc5b2c67c2990267832e9
John <john.dev.work.01@gmail.com>
2021-02-05 17:00:41 -0800

    Setup basic ws server

--- server/server.js
+++ server/server.js
@@ -1,11 +1,68 @@
 const http = require("http");
 const WebSocket = require("ws");
+const crypto = require("crypto");
 
 const PORT = 5000;
 
 const server = http.createServer();
 
-const wss = new WebSocket.Server({ server });
+function generateValue(key) {
+  return crypto
+    .createHash("sha1")
+    .update(key + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11", "binary")
+    .digest("base64");
+}
+
+server.on("upgrade", (req, socket) => {
+  if (req.headers["upgrade"] !== "websocket") {
+    socket.end("HTTP/1.1 400 Bad Request");
+    return;
+  }
+
+  const acceptKey = req.headers["sec-websocket-key"];
+  const acceptHash = generateValue(acceptKey);
+
+  const resHeaders = [
+    "HTTP/1.1 101 Web Socket Protocol Handshake",
+    "Connection: Upgrade",
+    "Upgrade: websocket",
+    `Sec-WebSocket-Accept: ${acceptHash}`,
+  ];
+
+  socket.write(resHeaders.join());
+});
+
+const wss = new WebSocket.Server({ server, clientTracking: true });
+
+let clientCounter = 0;
+
+wss.on("connection", function (ws, req) {
+  let fakeInterval;
+
+  ws.id = `client${clientCounter}`;
+  clientCounter++;
+
+  fakeInterval = streamFakeMessages(ws);
+
+  ws.on("message", function incoming(message) {
+    const msg = JSON.parse(message);
+
+    if (msg.cmd === "pause") {
+      // console.log("pausing interval: ", fakeInterval);
+      clearInterval(fakeInterval);
+    }
+    if (msg.cmd === "resume") {
+      fakeInterval = streamFakeMessages(ws);
+      // console.log("new interval:: ", fakeInterval);
+    }
+
+    console.log("received: ", message);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on port: ${PORT}`);
+});
 
 const fakeMessage = () => ({
   author_id: Math.round(Math.random() * 10000000),
@@ -13,19 +70,9 @@ const fakeMessage = () => ({
   tweet: "something",
 });
 
-wss.on("connection", function connection(ws) {
-  setInterval(() => {
-    ws.send(JSON.stringify(fakeMessage()));
+const streamFakeMessages = (ws) => {
+  return setInterval(() => {
+    const msg = fakeMessage();
+    ws.send(JSON.stringify(msg));
   }, 500);
-
-  ws.on("message", function incoming(message) {
-    console.log("received: %s", message);
-
-    // echo message
-    ws.send("received: " + message);
-  });
-});
-
-server.listen(PORT, () => {
-  console.log(`Server running on port: ${PORT}`);
-});
+};
